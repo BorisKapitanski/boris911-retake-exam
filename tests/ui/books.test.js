f0e774d345a62bb5f210e1f1db8701162806b3b8
Boris Kapitanski <boris911@abv.bg>
2023-12-13 12:26:26 +0200

    fix failing test

--- tests/ui/books.test.js
+++ tests/ui/books.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check books page', async ({ page }) => {
-    await page.goto('https://boris911-library.onrender.com');
+    await page.goto('https://boris911-library.onrender.com/books');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
